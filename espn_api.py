ed541a628ce7feb9a79da80bb2869ba3884ef3f8
Grey Fertich <greyfertich@gmail.com>
2022-01-11 01:08:23 -0500

    Can't parse tables all at once, so first working with rushing data

--- espn_api.py
+++ espn_api.py
@@ -6,15 +6,30 @@ class Espn_api:
     url_prefix = "https://www.espn.com/nfl/boxscore/_/gameId/"
     @classmethod
     def get_team_stats(cls, game_id):
+        print(game_id)
         team_stats_response = requests.get(url_prefix + str(game_id))
         team_stats_soup = BeautifulSoup(team_stats_response.text, 'html.parser')
-        tables = team_stats_soup.find_all("table", class_="mod-data")
-        table_data = [cls.get_table_data(table) for table in tables[:1]]
+        # tables = team_stats_soup.find_all("table", class_="mod-data")
+        # table_data = [cls.get_table_data(table) for table in tables]
+        table_data = cls.get_rushing_data(team_stats_soup)
+        # for table in table_data:
+        #     print(table)
     @classmethod
     def get_table_data(cls, table):
         tds = table.find_all("td")
-        for td in tds:
-            print(td)
-            print()
+        data = {}
+        try:
+            player_name = next(iter(set(tds[0].find_all("span")).difference(tds[0].find_all("span", class_="abbr")))).text
+            print(player_name)
+            for td in tds[1:]:
+                data[td['class'][0]] = td.text
+        except Exception as e:
+            player_name = "INVALID_NAME"
+        return player_name, data
+    @classmethod
+    def get_rushing_data(cls, soup):
+        rushing_data = soup.find("div", {"id":"gamepackage-rushing"})
+        away_data = rushing_data.find("div", class_="gamepackage-away-wrap")
+        print(away_data)
 
 Espn_api.get_team_stats(401326600)
