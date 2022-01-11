2475d5fe472303225f2acafa5a0dd354b3b60f1d
Grey Fertich <greyfertich@gmail.com>
2022-01-11 00:13:05 -0500

    updated espn api

--- espn_api.rb
+++ espn_api.rb
@@ -1,4 +1,6 @@
-
+require 'uri'
+require 'net/http'
+require 'nokogiri'
 
 class Espn_api
   def initialize
@@ -7,9 +9,23 @@ class Espn_api
   def get_team_stats(game_id)
     team_stats_endpoint = URI(@url_prefix + game_id.to_s)
     team_stats_response = Net::HTTP.get_response(team_stats_endpoint)
-    parsed_stats = Nokogiri::HTML.parse(team_stats_response)
+    parsed_stats = Nokogiri::HTML(team_stats_response.body)
+    # parsed_stats = Nokogiri::Slop(team_stats_response.body)
+    class_name = "mod-data"
+    @stats = parsed_stats.xpath("//*[@class=\"#{class_name}\"]")
+    count = 0
+    puts @stats.search("td")
+    for table in @stats.search("td") do
+      count += 1
+      puts table
+      puts "\n\n"
+    end
+    # puts "count " + count.to_s
+    # @stats = @stats.xpath("//td").first
 
+    # @stats = parsed_stats.xpath("//*[@class=\"#{class_name}\"]")
+  end
 end
 
-espn = Espn_api.new(401326600)
-puts espn.get_team_stats
+espn = Espn_api.new
+espn.get_team_stats(401326600)
