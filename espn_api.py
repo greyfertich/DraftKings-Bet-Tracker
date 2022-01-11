4c465919e730da5167e4fc428943ee16a8178213
Grey Fertich <greyfertich@gmail.com>
2022-01-11 15:57:57 -0500

    Added passing stats

--- espn_api.py
+++ espn_api.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from rushingtable import RushingTable
+from table import RushingTable, ReceivingTable, PassingTable
 
 class Espn_api:
     global url_prefix
@@ -13,6 +13,7 @@ class Espn_api:
         team_stats_soup = BeautifulSoup(team_stats_response.text, 'html.parser')
         rushing_data = cls.get_rushing_data(team_stats_soup)
         receiving_data = cls.get_receiving_data(team_stats_soup)
+        passing_data = cls.get_passing_data(team_stats_soup)
 
     @classmethod
     def get_table_data(cls, table):
@@ -72,5 +73,74 @@ class Espn_api:
         home_data = receiving_data.find("div", class_="gamepackage-home-wrap")
         home_data_table = home_data.find("table", class_="mod-data").find("tbody")
         home_data_table_rows  = set(home_data_table.find_all("tr")).difference(home_data_table.find_all("tr", class_="highlight"))
+
+        receiving_table = ReceivingTable()
+
+        for row in away_data_table_rows:
+            name_data = row.find("td", class_="name")
+            name = next(iter(set(name_data.find_all("span")).difference(name_data.find_all("span", class_="abbr")))).text
+            receptions = row.find("td", class_="rec").text
+            yards = row.find("td", class_="yds").text
+            average = row.find("td", class_="avg").text
+            touchdowns = row.find("td", class_="td").text
+            long = row.find("td", class_="long").text
+            targets = row.find("td", class_="tgts").text
+            receiving_table.add_away_row([name, receptions, yards, average, touchdowns, long, targets])
+
+        for row in home_data_table_rows:
+            name_data = row.find("td", class_="name")
+            name = next(iter(set(name_data.find_all("span")).difference(name_data.find_all("span", class_="abbr")))).text
+            receptions = row.find("td", class_="rec").text
+            yards = row.find("td", class_="yds").text
+            average = row.find("td", class_="avg").text
+            touchdowns = row.find("td", class_="td").text
+            long = row.find("td", class_="long").text
+            targets = row.find("td", class_="tgts").text
+            receiving_table.add_away_row([name, receptions, yards, average, touchdowns, long, targets])
+
+    @classmethod
+    def get_passing_data(cls, soup):
+        passing_data = soup.find("div", {"id":"gamepackage-passing"})
+
+        away_data = passing_data.find("div", class_="gamepackage-away-wrap")
+        away_data_table = away_data.find("table", class_="mod-data").find("tbody")
+        away_data_table_rows = set(away_data_table.find_all("tr")).difference(away_data_table.find_all("tr", class_="highlight"))
+
+        home_data = passing_data.find("div", class_="gamepackage-home-wrap")
+        home_data_table = home_data.find("table", class_="mod-data").find("tbody")
+        home_data_table_rows  = set(home_data_table.find_all("tr")).difference(home_data_table.find_all("tr", class_="highlight"))
+
+        passing_table = PassingTable()
+
+        for row in away_data_table_rows:
+            name_data = row.find("td", class_="name")
+            name = next(iter(set(name_data.find_all("span")).difference(name_data.find_all("span", class_="abbr")))).text
+            completions = row.find("td", class_="c-att").text
+            yards = row.find("td", class_="yds").text
+            average = row.find("td", class_="avg").text
+            touchdowns = row.find("td", class_="td").text
+            ints = row.find("td", class_="int").text
+            sacks = row.find("td", class_="sacks").text
+            qbr = row.find("td", class_="qbr").text
+            rtg = row.find("td", class_="rtg").text
+            passing_table.add_away_row([completions, yards, average, touchdowns, ints, sacks, qbr, rtg])
+
+        for row in home_data_table_rows:
+            name_data = row.find("td", class_="name")
+            name = next(iter(set(name_data.find_all("span")).difference(name_data.find_all("span", class_="abbr")))).text
+            completions = row.find("td", class_="c-att").text
+            yards = row.find("td", class_="yds").text
+            average = row.find("td", class_="avg").text
+            touchdowns = row.find("td", class_="td").text
+            ints = row.find("td", class_="int").text
+            sacks = row.find("td", class_="sacks").text
+            qbr = row.find("td", class_="qbr").text
+            rtg = row.find("td", class_="rtg").text
+            passing_table.add_away_row([completions, yards, average, touchdowns, ints, sacks, qbr, rtg])
+
+        for row in passing_table.home_rows:
+            print(row)
+        for row in passing_table.away_rows:
+            print(row)
 
 Espn_api.get_team_stats(401326595)
--- table.py
+++ table.py
@@ -1,8 +1,33 @@
-
-
 class StatTable:
     def __init__(self):
         self.home_rows = []
         self.away_rows = []
-    def add_row(self, data):
-        pass
+    def add_home_row(self, data):
+        self.home_rows.append(data)
+    def add_away_row(self, data):
+        self.away_rows.append(data)
+
+class RushingTable(StatTable):
+    def __init__(self):
+        super().__init__()
+        self.columns = ["NAME", "CAR", "YDS", "AVG", "TD", "LONG"]
+
+class ReceivingTable(StatTable):
+    def __init__(self):
+        super().__init__()
+        self.columns = ["REC", "YDS", "AVG", "TD", "LONG", "TGTS"]
+
+class PassingTable(StatTable):
+    def __init__(self):
+        super().__init__()
+        self.columns = ["C/ATT", "YDS", "AVG", "TD", "INT", "SACKS", "QBR", "RTG"]
+
+class FumbleTable(StatTable):
+    def __init__(self):
+        super().__init__()
+        self.columns = ["FUM", "LOST", "REC"]
+
+class DefensiveTable(StatTable):
+    def __init__(self):
+        super().__init__()
+        self.columns = ["TOT", "SOLO", "SACKS", "TFL", "PD", "QB HITS", "TD"]
