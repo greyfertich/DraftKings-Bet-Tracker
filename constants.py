69d2c1958c9f3c00cf4285feb5753ff320f6dad4
Grey Fertich <greyfertich@gmail.com>
2022-01-11 23:30:10 -0500

    Added constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+PASSING_ID = "gamepackage-passing"
+RUSHING_ID = "gamepackage-rushing"
+RECEIVING_ID = "gamepackage-receiving"
+FUMBLE_ID = "gamepackage-fumbles"
+DEFENSIVE_ID = "gamepackage-defensive"
+INTERCEPTION_ID = "gamepackage-interceptions"
+KICKRETURN_ID = "gamepackage-kickReturns"
+PUNTRETURN_ID = "gamepackage-puntReturns"
+KICKING_ID = "gamepackage-kicking"
+PUNTING_ID = "gamepackage-punting"
+
+TABLE_IDS = [PASSING_ID, RUSHING_ID, RECEIVING_ID, FUMBLE_ID, DEFENSIVE_ID,
+    INTERCEPTION_ID, KICKRETURN_ID, PUNTRETURN_ID, KICKING_ID, PUNTING_ID]
